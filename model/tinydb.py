d78a8562259a80c6d755e30b9513ab66fa4253f2
AlxandrV <a.vigneaud@gmail.com>
2021-12-06 14:45:34 +0100

    Add - add model TinyDB

---
+++ model/tinydb.py
@@ -0,0 +1,15 @@
+from tinydb import TinyDB, Query
+
+class TinyDb:
+
+    def insert():
+        pass
+
+    def search():
+        pass
+
+    def delete():
+        pass
+
+    def update():
+        pass
